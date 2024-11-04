7fddf00d40e533ba3bf29510cdd56b68fcc3ee20
추준범 <cjb3333@naver.com>
2024-11-04 17:18:36 +0900

    crispy_forms 적용

--- hello_django_prj/settings.py
+++ hello_django_prj/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django_extensions',
     'blog',
     'single_pages',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +129,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
