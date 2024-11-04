73eb1e175eeb506d934f71f29e47d2f3d1924f14
추준범 <cjb3333@naver.com>
2024-11-04 17:47:10 +0900

    content 입력란에 마크다운 적용

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Post, Category, Tag
+from markdownx.admin import MarkdownxModelAdmin
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', )}
@@ -7,7 +8,7 @@ class CategoryAdmin(admin.ModelAdmin):
 class TagAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', )}
 
-admin.site.register(Post)
+admin.site.register(Post, MarkdownxModelAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Tag, TagAdmin)
 
